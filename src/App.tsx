3d56ea1e439f3e2cc614b7a091197b6b7232514b
OwnKng <owen.king@outlook.com>
2022-07-31 10:27:58 +0100

    Initital effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import Sketch from "./Sketch"
 
 const App = () => (
   <div>
     <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 500] }}>
-      <OrbitControls />
       <Sketch />
     </Canvas>
   </div>
